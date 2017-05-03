4fc248a5af833e8e3aa2b179a77ec9977dce39b1
Hady Osman <hady.osman@xero.com>
2017-05-03 22:32:17 +1200

    Configure build step of dist assets

--- tests/infrastructure/wdio.conf.js
+++ tests/infrastructure/wdio.conf.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 
 const pactProvider = require('./pactProvider');
-const webpackConfig = require('../../webpack/integration-tests.config');
+const webpackConfig = require('../../webpack/config.integration-tests');
 
 const WEBPACK_DEV_SERVER_PORT = 3000;
 
--- webpack/base.config.js
+++ webpack/base.config.js
@@ -9,7 +9,7 @@ module.exports = template => ({
     path.join(__dirname, '../src/index.js')
   ],
   output: {
-    path: path.join(__dirname, '/dist/'),
+    path: path.join(__dirname, '../dist/'),
     filename: '[name].js',
     publicPath: '/'
   },
