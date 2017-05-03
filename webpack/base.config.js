fe43169927a2e05e0ec3eb42180f9ecccf11d5a2
Hady Osman <hady.osman@xero.com>
2017-05-03 22:45:35 +1200

    Fix public path link in webpack config

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -11,7 +11,7 @@ module.exports = template => ({
   output: {
     path: path.join(__dirname, '../dist/'),
     filename: '[name].js',
-    publicPath: '/'
+    publicPath: './'
   },
   devServer: {
     port: 3000,
